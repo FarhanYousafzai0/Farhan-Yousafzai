03bb1e58c0ca74c2d0a9c66ccf9ebb1c144b919e
Farhan Yousafzai <coderwithferry@gmail.com>
2025-06-16 01:26:35 +0500

    Look what i build

--- src/Components/Home/Nav.jsx
+++ src/Components/Home/Nav.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import gsap from 'gsap'
 
 const Nav = () => {
+  const [show, setShow] = useState(false);
+  const contactRef = useRef();
 
+ 
 
-
-
-  
   return (
     <>
-    <nav className='flex items-center justify-between h-[60px]  rounded-md p-5 '>
-
-{/* Logo */}
-<span className='cursor-pointer'>© Code By Farhan</span>
-
-<div className='flex items-center gap-5'>
-<Link to="/">Home</Link>
-<Link to="/about">About</Link>
-<Link to="/insights">Insights</Link>  
-<Link to="">Let’s Talk</Link> 
-
-
-
-</div>
-    </nav>
       
+      
+      <nav className='flex items-center justify-between h-[60px] rounded-md p-5 relative z-20'>
+        {/* Logo */}
+        <span className='cursor-pointer'>© Code By Farhan</span>
+
+        <div className='flex items-center gap-5'>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/insights">Insights</Link>  
+          <button 
+            o
+            className='cursor-pointer'
+          >
+            Let's Talk
+          </button> 
+        </div>
+      </nav>
     </>
   )
 }
